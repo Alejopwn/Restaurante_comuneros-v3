53aaffb5c4b8f47e54e3313f81110a3802346386
alejopwn <alejandro@example.com>
2026-06-18 08:38:24 -0500

    release: publicar la version 1.1.0

--- src/Modelo/AutoUpdater.java
+++ src/Modelo/AutoUpdater.java
@@ -6,7 +6,7 @@ import java.net.URL;
 
 public class AutoUpdater {
 
-    public static final String CURRENT_VERSION = "1.0.9";
+    public static final String CURRENT_VERSION = "1.1.0";
     private static final String VERSION_URL = "https://raw.githubusercontent.com/Alejopwn/Restaurante_comuneros-v3/main/version.txt";
 
     public interface UpdateProgressCallback {
